a36c91ebe292f8d0e7e377b6e3d99b196b90e868
kunalgiri12345 <kunalkumargiri@Kunals-MacBook-Air.local>
2024-02-11 10:19:56 +0530

    Added home route

--- src/app.js
+++ src/app.js
@@ -4,5 +4,7 @@ const app = express();
 app.get("/testNode", (_req, res) => {
     res.status(200).send("Yes the TestNode endpoint worked")
 })
-
+app.get("/home", (_req, res) => {
+    res.status(200).send("Welcome to home dashboard")
+})
 module.exports = app;
