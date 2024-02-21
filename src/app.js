caa27b67b4cb9456adf4d0250b2c11fc65fa0d94
kunalgiri12345 <kunalkumargiri@Kunals-MacBook-Air.local>
2024-02-22 00:11:04 +0530

    added contact route

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.get("/", (_req, res) => {
+    res.status(200).send("Welcome to home dashboard")
+})
 app.get("/testNode", (_req, res) => {
     res.status(200).send("Yes the TestNode endpoint worked")
 })
-app.get("/home", (_req, res) => {
-    res.status(200).send("Welcome to home dashboard")
+app.get("/contact", (_req, res) => {
+    res.status(200).send("Welcome to Contact dashboard")
 })
 module.exports = app;
